dbf44a90fe4542ce7486d16ded29d879ddb2f6fb
Ilya <Ilyagearasimchik@gmail.com>
2020-03-21 13:14:26 +0300

    Task 1

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -6,7 +6,12 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            //code here
-        }
+            int i;
+            for (i = 0; initialPopulation <= currentPopulation; i++)
+            {
+                initialPopulation += (initialPopulation * (int)percent /100) + visitors;
+            }
+            return i;
+        }  
     }
 }
